abc8f31d4afd1728a60cecb1cfa16a6cf6eeb5a7
Alex007-Biz <bizukin@yandex.ru>
2024-03-31 20:27:19 +0300

    4.2

---
+++ new2.py
@@ -0,0 +1,10 @@
+class Test():
+    def public_func(self):
+        print("Публичный метод")
+
+    def _protected_func(self):
+        print("Защищенный метод")
+
+
+    def __private_func(self):
+     print("Приватный метод")
