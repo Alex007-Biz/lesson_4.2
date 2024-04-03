0dabee4926e3bd16e95e294102815d019e34ee5d
Alex007-Biz <bizukin@yandex.ru>
2024-04-03 07:33:58 +0300

    Update new2.py

--- new2.py
+++ new2.py
@@ -1,21 +1,43 @@
-class Test():
-    def public_func(self):
-        print("Публичный метод")
+class User:
+    def __init__(self, user_id, name, access_level='user'):
+        self._user_id = user_id
+        self._name = name
+        self._access_level = access_level
 
-    def _protected_func(self):
-        print("Защищенный метод")
+    def get_user_id(self):
+        return self._user_id
+
+    def get_name(self):
+        return self._name
+
+    def get_access_level(self):
+        return self._access_level
 
 
-    def __private_func(self):
-     print("Приватный метод")
+class Admin(User):
+    def __init__(self, user_id, name):
+        super().__init__(user_id, name, 'admin')
+        self._users_list = []
 
-    def test_private(self):
-        self.__private_func()
+    def add_user(self, user):
+        self._users_list.append(user)
 
-test = Test()
-test.public_func()
-test._protected_func()
-test.test_private()
+    def remove_user(self, user):
+        self._users_list.remove(user)
 
+    def get_users_list(self):
+        return self._users_list
+
+# Пример использования классов
+user1 = User(1, 'John Doe')
+admin1 = Admin(2, 'Alice Smith')
+
+print(user1.get_name())
+print(admin1.get_access_level())
+
+admin1.add_user(user1)
+users_list = admin1.get_users_list()
+for user in users_list:
+    print(user.get_name())
 
 
